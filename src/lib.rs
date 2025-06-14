cce319fc4a8a895aedc42ad6297e27765647c344
이종경 <gyoung3063413@naver.com>
2025-06-14 18:34:14 +0900

    feat: add markdown to html utils

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,10 @@
 use wasm_bindgen::prelude::*;
+use pulldown_cmark::{Parser, html};
 
 #[wasm_bindgen]
-extern {
-    pub fn alert(s: &str);
-}
-
-#[wasm_bindgen]
-pub fn greet(name: &str) {
-    alert(&format!("Hello, {}!", name));
+pub fn md_to_html(markdown: &str) -> String {
+    let parser = Parser::new(markdown);
+    let mut html_out = String::new();
+    html::push_html(&mut html_out, parser);
+    html_out
 }
